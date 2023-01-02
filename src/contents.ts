1b887421111b9e201de2d0b00e11785979b9001c
omitsuhashi <osamu.mitsuhashi224@gmail.com>
2023-01-02 14:46:24 +0900

    feat block all sites

---
+++ src/contents.ts
@@ -0,0 +1,19 @@
+const overlay = (_title: string) => {
+  const overlayDiv = document.createElement('div')
+  const titleElement = document.createElement('p');
+  overlayDiv.className = 'block-wasting-overlay';
+  overlayDiv.style.zIndex = '2147483647';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.backgroundColor = 'white';
+  overlayDiv.style.position = 'fixed';
+  overlayDiv.style.visibility = 'visible';
+  overlayDiv.style.top = '0';
+  titleElement.textContent = _title;
+  overlayDiv.appendChild(titleElement);
+  document.body.appendChild(overlayDiv);
+}
+
+(() => {
+  overlay('dummy');
+})()
